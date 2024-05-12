4a736b5aa193dff6d3c2e7ff35a16544ad5eb2a0
JuanAsprillagithub <167722009+JuanAsprillagithub@users.noreply.github.com>
2024-05-12 17:38:45 -0500

    Update scripts.js
    ajuste carrito de compras

--- scripts.js
+++ scripts.js
@@ -1,12 +1,27 @@
+function verCarrito() {
+  // Encuentra el elemento donde quieres mostrar el carrito
+  var contenedorCarrito = document.getElementById('contenedorCarrito');
+  // Asegúrate de que el contenedor esté vacío antes de agregar los nuevos elementos
+  contenedorCarrito.innerHTML = '';
 
-var carrito = [];
-
-function agregarAlCarrito(idProducto) {
-  
+  // Recorre el carrito y crea los elementos HTML para cada producto
+  carrito.forEach(function(producto, index) {
+    var div = document.createElement('div');
+    div.classList.add('productoEnCarrito');
+    div.innerHTML = `
+      <p>${producto.nombre}</p>
+      <p>Precio: ${producto.precio}</p>
+      <button onclick="removerDelCarrito(${index})">Remover</button>
+    `;
+    contenedorCarrito.appendChild(div);
+  });
 }
 
-function verCarrito() {
-  
+function removerDelCarrito(index) {
+  // Remueve el producto del carrito
+  carrito.splice(index, 1);
+  // Actualiza la visualización del carrito
+  verCarrito();
 }
 
 
